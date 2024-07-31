eff68460249fc05b10479ac6ab30bba08ec3b42a
Thewsthews <114445432+Thewsthews@users.noreply.github.com>
2024-07-31 14:39:34 +0300

    Update Calculator.rb
    Added a few concepts to increase overal complexity of the program :)

--- Calculator.rb
+++ Calculator.rb
@@ -8,10 +8,17 @@ def calculate(num1, operator, num2)
         num1 * num2
     when '/'
         num1 != 0 ? num1 / num2 : "Errror: Division by zero"
+    when '^'
+        num1 ** num2
+    when '%'
+        num1 % num2
     else
         "Error: Invalid operator"
     end
 end
+
+history = []
+memory = 0
 
 puts "Welcome to the Simple Calculator By TT!"
 puts "Enter 'q' to quit."
@@ -21,21 +28,55 @@ loop do
     num1 = gets.chomp
     break ifnum1.downcase == 'q'
 
-    num1 = num1.to_f
+    if num1.downcase == 'ans' && !history.empty?
+        num1 = history.last[:result]
+    else
+        num1 = num1.to_f
+    end
+
 
     print "Enter operator (+, -, *, /): "
     operator = gets.chomp
     break if operator.downcase == 'q'
 
+    if operator.downcase == 'h'
+        puts "Calculation History: "
+        history.each_with_index { |calq, index| puts "#{index + 1}: #{calq[:num1]} #{calq[:operator]} #{calq[:num2]} #{calq[:result]}" }
+        next
+    elsif operator.downcase == 'm'
+        print "Memory operation (store/recall/clear): "
+        mem_op = gets.chomp.downcase
+        case mem_op
+        when 'store'
+            memory = num1
+            puts "Stored #{num1} in memory"
+        when 'recall'
+            puts "Memory value: {memory}"
+        when 'clear'
+            memory = 0
+            puts "Memory cleared"
+        next
+        end
+
     print "Enter second number: "
     num2 = gets.chomp
     break if num2.downcase == 'q'
 
-    num2 = num2.to_f
+    if num2.downcase == 'ans' && !history.empty?
+        num2 = history.last[:result]
+    else
+        num2 = num2.to_f
+    end
+
 
     result = calculate(num1, operator, num2)
     puts "Result: #{result}"
+
+    history << {num1: num1, operator: operator, num2: num2, result: result }
+    
     puts
 end
 
 puts "Thank you for using the Simple Calculator!"
+
+end
