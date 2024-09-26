3013f3be314975c99c354be7a26d6eb49b01fac8
Mathews Etiegni <etiegnim@gmail.com>
2024-09-26 16:38:50 +0300

    Update CalcRb.rs
    Added trigonometric functions

--- CalcRb.rs
+++ CalcRb.rs
@@ -1,4 +1,4 @@
-use std::os::raw::c_double;
+use std::f64::consts::PI;
 
 #[no_mangle]
 pub extern "C" fn factorial(n: u64) -> u64 {
@@ -23,6 +23,26 @@ pub extern "C" fn fibonacci(n: u64) -> u64 {
 }
 
 #[no_mangle]
-pub extern "C" fn complex_power(base: c_double, exponent: c_double) -> c_double {
+pub extern "C" fn power(base: f64, exponent: f64) -> f64{
     base.powf(exponent)
+}
+#[no_mangle]
+pub extern "C" fn gcd(mut a: u64, mut b: u64) -> u64{
+    while b!=0{
+        (a,b)=(b, a%b);
+    }
+    a
+}
+#[no_mangle]
+            pub extern "C" fn lcm(a: u64, b: u64){
+                a/gcd(a,b)*b
+            }
+#[no_mangle]
+pub extern "C" fn sin_degrees(degrees: f64) -> {
+    (degrees *PI/180.0).sin()
+}
+#[no_mangle]
+pub extern "C" fn tan_degrees(degrees: f64) -> {
+    (degrees *PI/180.0).tan()
+}
 }
